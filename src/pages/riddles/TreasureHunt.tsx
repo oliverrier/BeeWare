f599315ebd0cee3991ee2352aed4a095119eb1c6
oliverrier <olivier.verrier@ynov.com>
2020-12-06 23:16:01 +0100

    fix riddle and add cheat button

--- src/pages/riddles/TreasureHunt.tsx
+++ src/pages/riddles/TreasureHunt.tsx
@@ -12,8 +12,9 @@ import {
   IonLabel,
   IonRow,
 } from '@ionic/react';
-import { save } from 'ionicons/icons';
-import React, { useContext, useEffect, useState } from 'react';
+import { apertureOutline, navigateCircleOutline } from 'ionicons/icons';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import AppContext from '../../data/app-context';
 import { Riddle } from '../../models/Riddle';
 import { checkCode } from '../../utils/utils';
@@ -24,15 +25,17 @@ interface Position {
 }
 
 const TreasureHunt: React.FC<{ riddle: Riddle }> = (props) => {
-  const getLocation = async () => {
-    try {
-      position = await Geolocation.getCurrentPosition();
-      console.log('position: ', position);
-      setUserPosition({ ...position.coords });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const appCtx = useContext(AppContext);
+  const history = useHistory();
+
+  const [inputCode, setInputCode] = useState('');
+  const [userPosition, setUserPosition] = useState<Position | undefined>(
+    undefined
+  );
+  const [startPosition, setStartPosition] = useState<Position | undefined>(
+    undefined
+  );
+  const [win, setWin] = useState(false);
 
   const successfulRiddle = () => {
     if (!props.riddle || !checkCode('Tre4surE', inputCode)) return;
@@ -40,64 +43,93 @@ const TreasureHunt: React.FC<{ riddle: Riddle }> = (props) => {
     updateRiddle.isSuccess = true;
     updateRiddle.timeSec = (new Date().getTime() - timeStart.getTime()) / 1000;
     appCtx.updateRiddle(updateRiddle);
+    history.replace('/');
   };
 
-  const appCtx = useContext(AppContext);
+  const getLocation = async () => {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      setUserPosition({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+      if (!startPosition) {
+        setStartPosition(userPosition);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const [inputCode, setInputCode] = useState('');
-  const [userPosition, setUserPosition] = useState({} as Position);
-
-  let position: GeolocationPosition;
+  const cheat = () => {
+    setUserPosition({
+      latitude: 50,
+      longitude: 3,
+    });
+  };
 
   getLocation();
 
   const timeStart = new Date();
-  const startPosition = userPosition;
+
+  if (
+    !win &&
+    userPosition &&
+    startPosition &&
+    (userPosition.latitude !== startPosition.latitude ||
+      userPosition.longitude !== startPosition.longitude)
+  ) {
+    setWin(true);
+  }
 
   return (
     <IonContent fullscreen>
       <IonGrid>
-        <IonRow>
+        <IonRow className="ion-justify-content-center">
           <IonCard>
             <p>Cette zone changera en fonction de l'énigme. Chasse au trésor</p>
           </IonCard>
         </IonRow>
-        <IonRow>
+        <IonRow className="ion-justify-content-center">
           <IonCol>
-            <IonCard>
-              <p>Position de départ :</p>
-              {startPosition ? (
-                <p>
-                  {startPosition.latitude} - {startPosition.longitude}
-                </p>
-              ) : (
-                <p>Pas de coordonnées</p>
-              )}
-            </IonCard>
+            <p>Position de départ :</p>
+            {startPosition ? (
+              <p>
+                {startPosition.latitude} - {startPosition.longitude}
+              </p>
+            ) : (
+              <p>Pas de coordonnées</p>
+            )}
           </IonCol>
 
           <IonCol>
-            <IonCard>
-              <p>Position actuelle :</p>
-              {userPosition ? (
-                <p>
-                  {userPosition.latitude} - {userPosition.longitude}
-                </p>
-              ) : (
-                <p>Pas de coordonnées</p>
-              )}
-            </IonCard>
+            <p>Position actuelle :</p>
+            {userPosition ? (
+              <p>
+                {userPosition.latitude} - {userPosition.longitude}
+              </p>
+            ) : (
+              <p>Pas de coordonnées</p>
+            )}
           </IonCol>
         </IonRow>
-        <IonRow>
-          <IonCol size="1" offset="4">
-            <IonButton mode="ios" fill="outline" onClick={() => getLocation()}>
-              <IonIcon icon={save} />
+        <IonRow className="ion-justify-content-center">
+          <IonCol size="1">
+            <IonButton mode="ios" fill="outline" onClick={getLocation}>
+              <IonIcon icon={navigateCircleOutline} />
               <IonLabel>getLocation</IonLabel>
             </IonButton>
           </IonCol>
         </IonRow>
-        <IonRow>
+        <IonRow className="ion-justify-content-center">
+          <IonCol size="1">
+            <IonButton mode="ios" fill="outline" onClick={cheat}>
+              <IonIcon icon={apertureOutline} />
+              <IonLabel>cheat</IonLabel>
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow className="ion-justify-content-center">
           <IonItem>
             <IonInput
               className="ion-text-center"
@@ -109,6 +141,11 @@ const TreasureHunt: React.FC<{ riddle: Riddle }> = (props) => {
           </IonItem>
         </IonRow>
       </IonGrid>
+      {win && (
+        <IonItem color="primary">
+          <IonLabel className="ion-text-center">Le code est: Tre4surE</IonLabel>
+        </IonItem>
+      )}
     </IonContent>
   );
 };
